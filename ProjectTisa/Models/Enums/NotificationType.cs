ac424a662610fcbd0851ba284d35ef231fcbb245
Fetu56 <86706321+Fetu56@users.noreply.github.com>
2024-02-01 22:06:11 +0200

    Added models for notification system. #8

---
+++ ProjectTisa/Models/Enums/NotificationType.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectTisa.Models.Enums
+{
+    /// <summary>
+    /// Enum-type of <see cref="Notification"/>.
+    /// </summary>
+    public enum NotificationType
+    {
+        None,
+        Information,
+        Warning,
+        Error
+    }
+}
---
+++ ProjectTisa/Models/NotificationsUser.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectTisa.Models
+{
+    /// <summary>
+    /// List of notification for <see cref="User"/>.
+    /// </summary>
+    public class NotificationsUser
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; private init; }
+        public required int UserId { get; set; }
+        public required List<Notification> Notifications { get; set; }
+    }
+}
